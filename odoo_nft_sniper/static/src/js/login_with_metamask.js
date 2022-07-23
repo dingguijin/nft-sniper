1e3d09758091bcea32f2f3be4e4b9ca2a2559be4
Guijin Ding <dingguijin@gmail.com>
2022-07-24 00:18:23 +0800

    login with metamask

--- odoo_nft_sniper/static/src/js/login_with_metamask.js
+++ odoo_nft_sniper/static/src/js/login_with_metamask.js
@@ -7,8 +7,34 @@ import { useService } from "@web/core/utils/hooks";
 const loginWithMetamaskService = {
     dependencies: [],
     start(env, {}) {
-        console.log("loginWithMetamaskService");
+        console.log(env);
+        console.log(document);
+
+        document.addEventListener("DOMContentLoaded", function(event) {
+            console.log("DOMContentLoaded");
+        });
+
+        window.onload = function() {
+            console.log("WINDOW ONLOAD");
+        };
+
+        var metamask_login = document.getElementById("metamask-login");
+        metamask_login.addEventListener("click", function(event) {
+            console.log("click");
+            console.log(event);
+            event.preventDefault();
+        });
+        
+        env.bus.on("WEB_CLIENT_READY", null, async () => {
+            console.log("loginWithMetamask");
+            // jQuery(".metamask-login").on("click", function(event) {
+            //     event.preventDefault();
+            //     console.log("LOGIN BUTTON");
+            // });
+        });
     }
 };
 
 registry.category("services").add("loginWithMetamaskService", loginWithMetamaskService);
+
+
